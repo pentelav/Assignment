e1b2ac37d646b0308d02ff60cc06bd4ecc3786d8
pentelav <vpentela57844@ucumberlands.edu>
2025-03-16 23:04:11 -0500

    Create array.js

---
+++ array.js
@@ -0,0 +1,12 @@
+// JavaScript: Calculate the sum of an array with syntax errors
+function calculateSum(arr) {
+    let total = o;  // Error: 'o' should be '0'
+    for (let num of arr) {
+        total += num;
+    }
+    return total;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let result = calculate Sum (numbers); // Error: Incorrect function name spacing
+console.log("Sum in JavaScript:", result);
