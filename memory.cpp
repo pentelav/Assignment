857239161670428de6e3c79bec8dd2dbb5286505
pentelav <vpentela57844@ucumberlands.edu>
2025-04-13 12:34:52 -0500

    Delete memory.cpp

--- memory.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int* ptr = new int(10);
-    delete ptr;  // Manually freeing memory
-    cout << *ptr; // Error: Dangling pointer
-    return 0;
-}
