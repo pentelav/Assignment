6733223aa4d489fd13f2a8636b06fea7c6561cb4
pentelav <vpentela57844@ucumberlands.edu>
2025-04-13 12:35:00 -0500

    Delete memory.java

--- memory.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = new String("Hello");
-        str = null; // Eligible for garbage collection
-        System.gc(); // Suggest garbage collection
-    }
-}
